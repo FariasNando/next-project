dd75925b079c107b9a997f33b954cc8b79a814bb
Luís Fernando <lu.is.fernando@hotmail.com>
2025-12-16 09:31:47 -0300

    chore: initial

---
+++ components/menu.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+export default function Menu() {
+    return (
+        <div className="p-8 bg-zinc-900 text-white">
+            <ul className="flex space-x-4 text-zinc-600 dark:text-zinc-400">
+                <li>
+                    <Link href="/">Home</Link></li>
+                <li>
+                    <Link href="/about">About</Link></li>
+                <li>
+                    <Link href="/contact">Contact</Link></li>
+                <li>
+                    <Link href="/cursos">Cursos</Link></li>
+            </ul>
+        </div>
+    );
+}
